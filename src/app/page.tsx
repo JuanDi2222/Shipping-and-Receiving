dc49149dededb239223428e2e094b2198a83d078
Juan Silva  <juandiegosilvatello@gmail.com>
2024-05-09 10:08:40 -0600

    Pagina principal y rooting basico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,45 @@
 import Link from "next/link";
+import { lusitana } from '~/app/ui/fonts';
+import Image from 'next/image';
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-White-500 p-4 md:h-52">
+        <Image
+          src="/PHIN_BIG.png"
+          alt="Logo"
+          width={500}
+          height={100}
+          
+        />
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 antialiased md:text-3xl md:leading-normal`}
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
+            <strong>Proyecto en desarrollo</strong> para el MTC{' '}
+            <a href="https://phinia.com" className="text-red-500">
+              Phinia
+            </a>
+              , desarrollado por Juan c:
+          </p>
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
+            href="/Login"
+            className="flex items-center gap-5 self-start rounded-lg bg-red-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
+            <span>Log in</span> 
           </Link>
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          <Image
+            src="/BG_MAIN.jpg"
+            alt="Logo"
+            width={1000}
+            height={100}
+          />
         </div>
       </div>
     </main>
