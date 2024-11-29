b45eefc4b82f6c74e8d1d27fa1d8fd2c775bbe2e
JuanDi2222 <juandiegosilvatello@gmail.com>
2024-11-29 10:40:24 -0700

    Fix

--- src/app/ui/profile/profile-image.tsx
+++ src/app/ui/profile/profile-image.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 
 
-export default async function ProfileImage({user}) {
+export default async function ProfileImage({user}: {user: any}) {
   return (
     <Image src={user[0].image} alt="profile" width={600} height={600} />
 );
