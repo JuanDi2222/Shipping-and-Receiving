f1a499ccc72549a7cf84f2c38d41c3063855c9ec
JuanDi2222 <juandiegosilvatello@gmail.com>
2025-01-09 15:07:40 -0700

    Avisos

--- src/app/ui/admin/pdf.tsx
+++ src/app/ui/admin/pdf.tsx
@@ -4,23 +4,19 @@ import {
   Page,
   View,
   Text,
-  Image,
   PDFViewer,
   StyleSheet,
   Font,
 } from "@react-pdf/renderer";
 import { useState, useEffect } from "react";
 
-
 export function PDFView(data: any) {
   const notice = data.data;
-
-  
 
   Font.register({
     family: "rambla",
     src: "https://fonts.gstatic.com/s/abel/v6/N59kklKPso9WzbZH9jwJSg.ttf",
-  })
+  });
 
   const styles = StyleSheet.create({
     id: {
@@ -57,52 +53,127 @@ export function PDFView(data: any) {
     time: {
       margin: 30,
       fontSize: 16,
-      textAlign: 'justify',
-      fontFamily: 'rambla',
-
+      textAlign: "justify",
+      fontFamily: "rambla",
     },
     text: {
       fontSize: 16,
-      textAlign: 'justify',
-      fontFamily: 'rambla', 
+      textAlign: "justify",
+      fontFamily: "rambla",
       margin: 10,
-    }
-  })
+    },
+    table: {
+      width: "auto",
+      margin: 10,
+      borderStyle: "solid",
+      borderWidth: 1,
+      borderColor: "#000",
+    },
+    tableRow: {
+      flexDirection: "row",
+    },
+    tableCol: {
+      width: "50%",
+      borderStyle: "solid",
+      borderWidth: 1,
+      borderColor: "#000",
+      padding: 5,
+    },
+    tableHeader: {
+      fontSize: 14,
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    tableCell: {
+      fontSize: 12,
+      textAlign: "center",
+    },
+  });
+
   
-    const PDF = () => {
-      return (
-        <Document>
-          <Page size="A4" >
-            <View style={styles.id}>
-              <Text>No: {notice.id}</Text>
+
+  const destinies = [
+    { name: "EPDC", key: "epdc", bulkKey: "bulksepdc" },
+    { name: "ETDC", key: "etdc", bulkKey: "bulksetdc" },
+    { name: "DHL", key: "dhl", bulkKey: "bulksdhl" },
+    { name: "FedEx", key: "fedex", bulkKey: "bulks" },
+    { name: "UPS", key: "ups", bulkKey: "bulksups" },
+    { name: "FedEx Ground", key: "fedexground", bulkKey: "bulksfdxground" },
+    { name: `${notice.other}`, key: "other", bulkKey: "bulksOther" },
+  ];
+
+  const renderTableRows = () => {
+    return destinies
+      .filter((destiny) => notice[destiny.key])
+      .map((destiny, index) => (
+        <View key={index} style={styles.tableRow}>
+          <View style={styles.tableCol}>
+            <Text style={styles.tableCell}>{destiny.name}</Text>
+          </View>
+          <View style={styles.tableCol}>
+            <Text style={styles.tableCell}>
+              {notice[destiny.bulkKey] ?? "N/A"}
+            </Text>
+          </View>
+        </View>
+      ));
+  };
+
+  const PDF = () => {
+    return (
+      <Document>
+        <Page size="A4">
+          <View style={styles.id}>
+            <Text>No: {notice.id}</Text>
+          </View>
+          <View>
+            <Text style={styles.title}>
+              PHINIA TECHNOLOGIES HOLDINGS MEXICO S DE RL DE CV
+            </Text>
+            <Text style={styles.subtitle}>
+              Ave. Hermanos Escobar #5756-B Col. Fovissste Chamizal, CP. 32310
+              Cd. Juárez, Chih.
+            </Text>
+            <Text style={styles.title2}>AVISO DE EMBARQUE MTC</Text>
+            <Text style={styles.time}>
+              Fecha: {new Date(notice.date).toLocaleDateString()}                                                                               Hora:{" "}
+              {new Date(notice.date).toLocaleTimeString()}
+            </Text>
+            <Text style={styles.text}>Placas: {notice.plates}</Text>
+            <Text style={styles.text}>Linea de Transporte: {notice.line}</Text>
+            <Text style={styles.text}>Bultos: {notice.bulks}                                                                                          Descripcion: {notice.description}</Text>
+            <Text style={styles.text}>Sello#: 0{notice.seal}</Text>
+            <Text style={styles.text}>
+              Manifiestos de embarque: {notice.manifest}
+            </Text>
+
+            <View style={styles.table}>
+              <View style={styles.tableRow}>
+                <View style={styles.tableCol}>
+                  <Text style={styles.tableHeader}>Destino</Text>
+                </View>
+                <View style={styles.tableCol}>
+                  <Text style={styles.tableHeader}>Bultos</Text>
+                </View>
+              </View>
+              {renderTableRows()}
             </View>
-            <View  >
-              <Text style={styles.title}>PHINIA TECHNOLOGIES HOLDINGS MEXICO S DE RL DE CV</Text>
-              <Text style={styles.subtitle}>Ave. Hermanos Escobar #5756-B Col. Fovissste Chamizal, CP. 32310 Cd. Juárez, Chih.</Text>
-              <Text style={styles.title2}>AVISO DE EMBARQUE MTC</Text>
-              <Text style={styles.time}>Fecha: {notice.date.toLocaleDateString()}                                                                                Hora:  {notice.date.toLocaleTimeString()}</Text>
-              <Text style={styles.text}>Linea de Transporte: {notice.line}</Text>
-              <Text style={styles.text}>Bultos: {notice.bulks}</Text>
-              <Text style={styles.text}>Nombre del Operador: {notice.operator}</Text>
-              <Text style={styles.text}>Generado por: {notice.creator}</Text>
-              <Text style={styles.text}>Sello#: 0{notice.seal}</Text>
-              <Text style={styles.text}>Placas: {notice.plates}</Text>
-              <Text style={styles.text}>Manifiestos de embarque: {notice.manifest}</Text>
+            <Text style={styles.text}>_____________________________</Text>
 
-           </View>
-          </Page>
-        </Document>
-      );
-    };
-
+            <Text style={styles.text}>Nombre del Operador: {notice.operator}                                 Generado por: {notice.creator}</Text>
+          </View>
+        </Page>
+      </Document>
+    );
+  };
 
   const [client, setClient] = useState(false);
   useEffect(() => {
     setClient(true);
   }, []);
+
   if (client) {
     return (
-      
       <PDFViewer
         className="w-full"
         showToolbar={true}
